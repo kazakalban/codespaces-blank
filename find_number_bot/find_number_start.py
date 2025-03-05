afd2d1c7a96979ff4e1322bbb687f773ea0c413d
kazakalban <kasymsauyt@gmail.com>
2025-03-05 02:47:11 +0000

    Написал немножко свой вариант игры угадай число но не полное может в будещем понадбится

--- find_number_bot/find_number_start.py
+++ find_number_bot/find_number_start.py
@@ -11,7 +11,21 @@ dp = Dispatcher()
 # этот хендлер будет срабатывать на команду "/start"
 @dp.message(Command("start"))
 async def start_hendler(message: Message):
-    await message.answer('hi')
+    await message.answer('Чтобы Согласиться поиграть с ботом в игру, отправив в чат "Да" или "Давай", или "Сыграем"')
+
+# этот хендлер будет срабатывать на команду "/help"
+@dp.message(Command("help"))
+async def help_hendler(message: Message):
+    await message.answer('Помощь')
+
+@dp.message()
+async def agree_game(message: Message):
+    if message.text.lower() in ["да","давай","сыграем"]:
+        await message.answer(text="Игра начинается!!!")
+    elif message.text.lower() in [ "нет","не хочу","в другой раз"]:
+        await message.answer(text="Игра не начинается!!!")
+    else:
+        await message.answer(text="Я не понял тебя")
 
 if __name__ == '__main__':
     dp.run_polling(bot)
