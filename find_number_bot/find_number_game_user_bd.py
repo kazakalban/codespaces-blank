6e286ae717534a29f68ad8ac5b1f707f21dca691
kazakalban <kasymsauyt@gmail.com>
2025-03-06 09:51:22 +0000

    Данные пользователеи

---
+++ find_number_bot/find_number_game_user_bd.py
@@ -0,0 +1,6 @@
+#Словарь в котором будет храниться данные пользователя
+user = {'in_game':False,
+        'secret_number': None,
+        'attempts': None,
+        'total_games': 0,
+        'wins': 0}
