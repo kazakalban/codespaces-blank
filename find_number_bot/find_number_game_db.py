10c065bb3d613a2f2bcf3d7c40730a08f58ea6e2
kazakalban <kasymsauyt@gmail.com>
2025-03-10 10:45:13 +0000

    По второму видео не работает надо сделать как первый

---
+++ find_number_bot/find_number_game_db.py
@@ -0,0 +1,62 @@
+import sqlite3
+import os
+
+
+# Определяем путь к базе данных внутри db_lession иначе anketa.db создается в корне
+DB_PATH = os.path.join(os.path.dirname(__file__), "anketa.db")
+
+
+__connection = None
+
+
+# Проверяет соединение 
+def get_connection():
+    global __connection
+    if __connection is None:
+        __connection = sqlite3.connect(DB_PATH)
+    return __connection
+
+
+def db_start(force: bool = False):
+    """ Проверить что нужные таблицы существют, иначе создать их
+        Важно: миграция на такие табличы вы должны производить самостоятельно!
+        :param force: явно пересоздать все таблицы
+    """
+    try:
+        conn = get_connection()  # Используем полный путь
+        print("Подключение успешно:", DB_PATH)  # Отладочный вывод
+
+        c = conn.cursor()
+        c.execute('''
+            CREATE TABLE IF NOT EXISTS find_number_bot_user_data (
+                user_id INTEGER PRIMARY KEY,
+                is_bot BOOLEAN NOT NULL,
+                first_name TEXT,
+                last_name TEXT,
+                username TEXT,
+                type TEXT NOT NULL
+            )
+        ''')
+
+        conn.commit()
+
+        print("База данных и Таблицы созданы если нет и записыны данные!")
+
+    except sqlite3.Error as e:
+        print("Ошибка при подключении к базе данных:", e)
+
+    # Сохранить изменения
+    conn.commit()
+
+
+# Добавлет в базу данные 
+""" def add_user(user_id: int, is_bot: bool, first_name:str, last_name:str, username:str, type:str):
+    conn = get_connection()
+    c = conn.cursor()
+    c.execute('INSERT INTO find_number_bot_user_data (user_id, is_bot, first_name, last_name, username, type) VALUES (?, ?, ?, ?, ?, ?)', (user_id, is_bot, first_name, last_name, username, type))
+    conn.commit()"""
+
+if __name__ == '__main__':
+    db_start()
+
+    #add_user(user_id=77788, is_bot=False, username='kass', first_name='Kassym', last_name='Sauyt', type=False)
---
+++ find_number_bot/find_number_game_with_db.py
@@ -0,0 +1,169 @@
+"""
+Игра "Угадай число" для одного пользователя
+Давайте для простоты пока предположим, что у нас всего один пользователь бота. 
+Нам понадобится хранить следующие данные:
+
+Состояние "Игра" или "Не игра"
+Загаданное ботом число
+Количество оставшихся у пользователя попыток
+Сколько всего было игр у пользователя
+В скольких играх пользователь выиграл
+"""
+
+from aiogram import Bot, Dispatcher, F
+from aiogram.filters import Command, CommandStart
+from aiogram.types import Message
+
+from private_for_API import BOT_TOKEN
+import find_number_game_text as texts
+from find_number_game_user_bd import users
+from find_number_game_tech import ATTEMPTS,get_random_number,STICKER_ID
+
+from find_number_game_db import db_start
+import asyncio
+
+
+#Создаем объекты бота 
+bot = Bot(BOT_TOKEN)
+dp = Dispatcher()
+
+
+
+async def on_startup(_):
+    print("on_startup вызван")
+    await db_start()
+    print('Бот успешно запущен!')
+
+
+# Этот хендлер будет срабатывать на команду "/start"
+@dp.message(CommandStart())
+async def process_start_command(message: Message):
+    await bot.send_sticker(message.chat.id, STICKER_ID)
+    await message.answer(
+        texts.START_TEXT,
+        parse_mode='Markdown'
+    )
+    if message.from_user.id not in users:
+        users[message.from_user.id] = {
+            'in_game': False,
+            'secret_number': None,
+            'attempts': None,
+            'total_games': 0,
+            'wins': 0
+        }
+
+# Этот хендлер будет срабатывать на команду "/help"
+@dp.message(Command(commands='help'))
+async def proccess_help_command(message:Message):
+    await message.answer(
+        texts.HELP_TEXT.format(ATTEMPTS = ATTEMPTS),
+        parse_mode='Markdown'
+    )
+
+
+# Этот хендлер будет срабатывать на команду "/stat"
+@dp.message(Command(commands='stat'))
+async def process_stat_command(message:Message):
+    await message.answer(
+        texts.STAT_TEXT.format(total_games = users[message.from_user.id]['total_games'],
+                        wins = users[message.from_user.id]['wins']),
+        parse_mode="Markdown"
+    )
+
+
+# Этот хэндлер будет срабатывать на команду "/cancel"
+@dp.message(Command(commands='cancel'))
+async def proccess_cancel_command(message: Message):
+    if users[message.from_user.id]['in_game']:
+        users[message.from_user.id]['in_game'] = False
+        await message.answer(
+            texts.CANCEL_TEXT,
+            parse_mode="Markdown"
+        )
+    else:
+        await message.answer(
+            texts.CANCEL_TEXT_ELSE,
+            parse_mode="Markdown"        
+        )
+
+
+# Этот хендлер будет срабатывать на согласие пользователя сыграть в игру
+@dp.message(F.text.lower().in_ (texts.POSITIVE_ANSWER))
+async def proccess_positive_answer(message: Message):
+    if not users[message.from_user.id]['in_game']:
+        users[message.from_user.id]['in_game'] = True
+        users[message.from_user.id]['secret_number'] = get_random_number()
+        print(users[message.from_user.id]['secret_number'])
+        users[message.from_user.id]['attempts'] = ATTEMPTS
+        await message.answer(
+            texts.POSITIVE_ANSWER_TEXT
+        )
+    else:
+        await message.answer(
+            texts.POSITIVE_ANSWER_TEXT_ELSE 
+        )
+
+
+# Этот хендлер будет срабатывать на отправку пользователем чисел от 1 до 100
+@dp.message(lambda x: x.text and x.text.isdigit() and 1<= int(x.text) <= 100)
+async def proccess_number_answer(message: Message):
+    if users[message.from_user.id]['in_game']:
+        if int(message.text) == users[message.from_user.id]['secret_number']:
+            users[message.from_user.id]['in_game'] = False
+            users[message.from_user.id]['total_games'] += 1
+            users[message.from_user.id]['wins'] += 1
+            await message.answer(
+                texts.NUMBER_ANSWER_TEXT,
+                parse_mode="Markdown"
+            )
+        elif int(message.text) > users[message.from_user.id]['secret_number']:
+            users[message.from_user.id]['attempts'] -= 1
+            await message.answer(texts.MY_NUMBER_LESS)
+        elif int(message.text) < users[message.from_user.id]['secret_number']:
+            users[message.from_user.id]['attempts'] -= 1
+            await message.answer(texts.MY_NUMBER_MORE)
+        if users[message.from_user.id]['attempts'] == 0:
+            users[message.from_user.id]['in_game'] = False
+            users[message.from_user.id]['total_games'] += 1
+            await message.answer(
+                texts.NUMBER_ANSWER_TEXT_NO_LIFE.format(secret_number = users[message.from_user.id]['secret_number']),
+                parse_mode="Markdown"
+            )
+    else:
+        await message.answer(texts.NUMBER_ANSWER_TEXT_IN_GAME_FALSE)
+
+
+# Этот хендлер будет срабатывать на отказ пользователя сыграть в игру
+@dp.message(F.text.lower().in_ (texts.NEGATIVE_ANSWER))
+async def proccess_negative_answer(message: Message):
+    if not users[message.from_user.id]['in_game']:
+        await message.answer(
+            texts.NO_ANSWER_TEXT,
+            parse_mode = "Markdown"
+        )
+    else:
+        await message.answer(
+            texts.OTHER_ANSWER_TEXT,
+            parse_mode="Markdown"
+        )
+
+
+# Этот хендлер будет срабатывать на остальные любые соощения
+@dp.message()
+async def proccess_other_answers(message: Message):
+        if users[message.from_user.id]['in_game']:
+            await message.answer(
+                texts.OTHER_ANSWER_TEXT,
+                parse_mode = "Markdown"
+            )
+        else:
+            await message.answer(
+                texts.OTHER_ANSWER_TEXT_ELSE,
+                parse_mode="Markdown"
+            ) 
+
+async def main():
+    await dp.start_polling(bot, on_startup=on_startup)
+
+if __name__ == '__main__':
+    asyncio.run(main())
